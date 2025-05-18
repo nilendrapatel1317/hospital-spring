d2fb153eed5429a9cc6620305801d0d125e0e428
Nilendra Patel <nilendrapatel1317@gmail.com>
2025-05-19 00:46:55 +0530

    Appointment error fixed

--- src/main/java/com/spring/serviceImplements/AppointmentServiceImplementation.java
+++ src/main/java/com/spring/serviceImplements/AppointmentServiceImplementation.java
@@ -55,7 +55,7 @@ public class AppointmentServiceImplementation implements AppointmentService {
 	@Override
 	public Optional<Appointment> getAppointmentById(String id) {
 		// TODO Auto-generated method stub
-		return Optional.empty();
+		return appointmentRepository.findById(id);
 	}
 
 	@Override
